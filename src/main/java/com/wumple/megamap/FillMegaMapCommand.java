d7010018c06809f41557030cd206fbecfeb32332
Jason Spangler <jasons@wumple.com>
2018-10-19 16:47:18 -0500

    Fixed #1 Command fillmegamap incorrectly overrides

--- src/main/java/com/wumple/megamap/FillMegaMapCommand.java
+++ src/main/java/com/wumple/megamap/FillMegaMapCommand.java
@@ -6,20 +6,19 @@ import java.util.List;
 import com.wumple.megamap.megamap.ItemMegaMap;
 import com.wumple.util.base.misc.Util;
 
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
-import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.MapData;
 
-public class FillMegaMapCommand implements ICommand
+public class FillMegaMapCommand extends CommandBase
 {
     private final List<String> aliases;
     
@@ -31,12 +30,6 @@ public class FillMegaMapCommand implements ICommand
         aliases.add("fm"); 
     }
     
-    @Override
-    public int compareTo(ICommand arg0)
-    {
-        return 0;
-    }
-
     @Override
     public String getName()
     {
@@ -92,6 +85,14 @@ public class FillMegaMapCommand implements ICommand
             sender.sendMessage(new TextComponentTranslation("command.megamap.fillmegamap.filledmap"));
         }
     }
+    
+    @Override
+    public int getRequiredPermissionLevel()
+    {
+        // TODO what do the level values mean?
+        // Maybe https://minecraft.gamepedia.com/Server.properties
+        return 4;
+    }
 
     @Override
     public boolean checkPermission(MinecraftServer server, ICommandSender sender)
@@ -99,22 +100,10 @@ public class FillMegaMapCommand implements ICommand
         EntityPlayer player = Util.as(sender.getCommandSenderEntity(), EntityPlayer.class);
         if (player != null)
         {
+            //return super.checkPermission(server, sender);
             return player.isCreative();
         }
         
         return false;
     }
-
-    @Override
-    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos targetPos)
-    {
-        return null;
-    }
-
-    @Override
-    public boolean isUsernameIndex(String[] args, int index)
-    {
-        return false;
-    }
-
 }
